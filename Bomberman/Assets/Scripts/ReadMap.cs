e0a8792317fe16c9cac09d8291da639edfb15b49
a <a>
2022-05-24 15:47:02 -0300

    ReadMap

---
+++ Bomberman/Assets/Scripts/ReadMap.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class ReadMap : MonoBehaviour
+{
+    [SerializeField] private GameObject pared;
+    [SerializeField] private GameObject vacio;
+
+    private int offset = 0;
+
+    private void Start()
+    {
+        ReadFileMap();
+    }
+
+    public void ReadFileMap()
+    {
+        string filePath = @"Assets/Maps/Map.txt";
+
+        List<string> lines = new List<string>();
+        lines = File.ReadAllLines(filePath).ToList();
+
+        foreach (string line in lines)
+        {
+            offset += 2;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'X')
+                {
+                    Instantiate(pared, new Vector3(i * 2.0f, offset + 0, 0), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(vacio, new Vector3(i * 2.0f, offset + 0, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
