2af5fc490236f4a8261682ea527bda63b1ed70dc
Matias <matiasmonti2011@hotmail.com>
2022-06-01 22:28:23 -0300

    Add player anim

--- Bomberman/Assets/Scripts/BombController.cs
+++ Bomberman/Assets/Scripts/BombController.cs
@@ -19,6 +19,10 @@ public class BombController : MonoBehaviour
     [SerializeField] private float explosionDuration = 1f;
     [SerializeField] private int explosionRadius = 1;
 
+    [Header("Destructible")]
+    [SerializeField] private GameObject destructible;
+    [SerializeField] private Destructible destructiblePrefab;
+
     private void OnEnable()
     {
         bombRemaining = bombAmount;
@@ -34,46 +38,58 @@ public class BombController : MonoBehaviour
 
     private IEnumerator PlaceBomb()
     {
+        //Redondeo la pos a un entero
         Vector2 position = transform.position;
         position.x = Mathf.Round(position.x);
         position.y = Mathf.Round(position.y);
 
+        //Instancio bomba y resto 1
         GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
         bombRemaining--;
 
+        //Espero el tiempo
         yield return new WaitForSeconds(bombFuseTime);
 
+        //Redondeo la pos de la bomba para pasarsela a la explosion
         position = bomb.transform.position;
         position.x = Mathf.Round(position.x);
         position.y = Mathf.Round(position.y);
 
+        //Instancio la explosion
         Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        explosion.SetActiveRenderer(explosion.start);
-        explosion.DestroyAfter(explosionDuration);
+        explosion.SetActiveRenderer(explosion.start);   //Arranca la primera animacion
+        explosion.DestroyAfter(explosionDuration);      //Despues de ciertos segundos se destruye la explosion
 
+        //Indico hacia donde explota
         Explode(position, Vector2.up, explosionRadius);
         Explode(position, Vector2.down, explosionRadius);
         Explode(position, Vector2.right, explosionRadius);
         Explode(position, Vector2.left, explosionRadius);
 
+        //Destruyo la bomba y sumo 1 mas
         Destroy(bomb);
         bombRemaining++;
     }
 
     private void Explode(Vector2 position, Vector2 direction, int length)
     {
+        //Me aseguro que pueda explotar hacia los costados
         if (length <= 0)
             return;
 
+        //Aseguro el lado y la posicion en cual explota
         position += direction;
 
+        //Reviso que la layermask sea la indicada y pueda explotar en caso de que no retorna
         if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
         {
+            //ClearDestructible(position);
             return;
         }
 
+        //Instancio la explosion en la ultima posicion de la bomba
         Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);     //Si ya paso la primera animacion entonces va la segunda
         explosion.SetDirection(direction);
         explosion.DestroyAfter(explosionDuration);
 
@@ -82,9 +98,18 @@ public class BombController : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //Muevo la bomba una vez que salgo
         if (collision.gameObject.CompareTag("Bomb"))
         {
             collision.isTrigger = false;
         }
     }
+
+    //private void ClearDestructible(Vector2 pos)
+    //{
+    //    if (destructible != null)
+    //    {
+    //        Instantiate(destructiblePrefab, pos, Quaternion.identity);
+    //    }
+    //}
 }
