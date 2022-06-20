fe18b3f3aa86625ff055da1bae3fd3ea69171107
Matias <matiasmonti2011@hotmail.com>
2022-06-20 14:04:06 -0300

    Add Audio

---
+++ Bomberman/Assets/Scripts/MenuScripts/AudioManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Slider slider;
+
+    private string volume = "volume";
+    private float musicVolume = 1f;
+
+
+    private void Awake()
+    {
+        SaveDataMute();
+    }
+
+    void Update()
+    {
+        MuteAudio();
+        audioSource.volume = musicVolume;
+    }
+
+    private void SaveDataMute()
+    {
+        if (PlayerPrefs.HasKey(volume))
+        {
+            if (PlayerPrefs.GetFloat(volume) > 0.0f)
+            {
+                toggle.isOn = true;
+            }
+            else
+            {
+                toggle.isOn = false;
+            }
+            slider.value = PlayerPrefs.GetFloat(volume, musicVolume);
+        }
+    }
+
+    private void MuteAudio()
+    {
+        if (toggle.isOn)
+        {
+            AudioListener.volume = audioSource.volume;
+            PlayerPrefs.SetFloat(volume, audioSource.volume);
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            PlayerPrefs.SetFloat(volume, 0);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void UpdateVolume(float volumeU)
+    {
+        musicVolume = volumeU;
+    }
+}
